193033ca5b7b546e35431e644c87b22488e9d947
Keonhwi99 <robert1995@naver.com>
2026-04-14 16:05:51 +0900

    이미지 원복

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+const basePath = process.env.PAGES_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath: basePath || undefined,
   images: { unoptimized: true },
+  env: { NEXT_PUBLIC_BASE_PATH: basePath },
 };
 
 export default nextConfig;
